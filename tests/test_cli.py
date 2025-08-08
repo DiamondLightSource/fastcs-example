c0ab7c071d3c4027ad8fc67ab77b83e54a6307df
Giles Knap <giles.knap@diamond.ac.uk>
2025-08-08 11:23:24 +0000

    fix version test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,4 @@
+import re
 import subprocess
 import sys
 
@@ -6,4 +7,8 @@ from fastcs_example import __version__
 
 def test_cli_version():
     cmd = [sys.executable, "-m", "fastcs_example", "--version"]
-    assert subprocess.check_output(cmd).decode().strip() == __version__
+    versions = subprocess.check_output(cmd).decode().strip()
+
+    my_version = re.findall("TemperatureController: (.*)", versions)
+    assert len(my_version) == 1
+    assert my_version[0] == __version__
